a893f9de3bf478f9b1d5c8f80d98d60befb373d1
Roberto Gomes <83621531+Robjunio@users.noreply.github.com>
2025-01-20 19:12:59 -0400

    Add Multiplayer
    Now its possible to enter with 4 player with diferents controllers (Xbox controller, PS4 controller and Keyboard).

--- 3D-PVP/Assets/Scripts/Player/PlayerMovement.cs
+++ 3D-PVP/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,31 +7,71 @@ namespace Player
     public class PlayerMovement : MonoBehaviour
     {
         private Rigidbody _rb;
-        private PlayerInput _playerInput;
 
-        private float _movementSpeed;
-        private float _dashForce = 500;
+        [SerializeField] private float _movementSpeed = 5f;
+        [SerializeField] private float _dashForce = 8;
         private float _turnSpeed = 720;
         private Vector3 _input;
-        private float _currentAngle;
 
         private bool _canDash = true;
         private float _timmer;
         private float _dashCooldown = 0.5f;
 
-        public void SetInfo(PlayerInput input, Rigidbody rb, float movementSpeed)
+        private void Awake()
         {
-            _playerInput = input;
+            TryGetComponent(out _rb);
+        }
+
+        public void SetInfo(Rigidbody rb, float movementSpeed)
+        {
             _movementSpeed = movementSpeed;
             _rb = rb;
+        }
 
-            _playerInput.gameActions.Player.Dash.performed += OnDash;
+        public void GetMovement(InputAction.CallbackContext ctx)
+        {
+            Debug.Log("Movement Caught");
+            Vector2 dir = ctx.ReadValue<Vector2>();
+
+            _input = new Vector3(dir.x, 0, dir.y);
+        }
+
+        public void OnDash(InputAction.CallbackContext context)
+        {
+            if (!_canDash)
+            {
+                var diff = Time.time - _timmer;
+                if (diff > _dashCooldown)
+                {
+                    _canDash = true;
+                    OnDash(context);
+                }
+                return;
+            }
+
+            _rb.velocity = Vector3.zero;
+            if (_input != Vector3.zero)
+            {
+                var rot = Quaternion.LookRotation(_input, Vector3.up);
+
+                transform.rotation = rot;
+            }
+
+            _rb.AddForce(transform.forward * _dashForce, ForceMode.Impulse);
+
+            _timmer = Time.time;
+            _canDash = false;
+            _rb.mass = 10;
+
+            transform.DOScale(transform.localScale * 1.5f, 0.4f).onComplete = () =>
+            {
+                transform.DOScale(Vector3.one, 0.05f);
+                _rb.mass = 1;
+            };
         }
 
         private void Update()
         {
-            _input = _playerInput.GetMovement();
-
             Look();
         }
 
@@ -43,51 +84,15 @@ namespace Player
         {
             if (_input == Vector3.zero) return;
 
-            var rot = Quaternion.LookRotation(transform.position, Vector3.up);
+            var rot = Quaternion.LookRotation(_input, Vector3.up);
 
             transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, _turnSpeed * Time.deltaTime);
         }
 
         private void Move()
         {
-            var lerpPos = Vector3.Lerp(transform.position, transform.position * _movementSpeed * Time.deltaTime, 0.8f);
-            _rb.velocity = Vector3.Lerp(_rb.velocity, new Vector3(0, _rb.velocity.y, 0), 0.1f);
+            var lerpPos = transform.position + _input * _movementSpeed * Time.deltaTime;
             _rb.MovePosition(lerpPos);
-        }
-
-        private void OnDash(InputAction.CallbackContext context)
-        {
-            if (!_canDash)
-            {
-                var diff = Time.time - _timmer;
-                if (diff > _dashCooldown) { 
-                    _canDash = true;
-                    OnDash(context);
-                }
-                return;
-            }
-
-            _rb.velocity = Vector3.zero;
-            if (_input != Vector3.zero)
-            {
-                var rot = Quaternion.LookRotation(transform.position, Vector3.up);
-
-                transform.rotation = rot;
-            }
-            _rb.AddForce(transform.forward * _dashForce, ForceMode.Impulse);
-            _timmer = Time.time;
-            _canDash = false;
-        }
-
-        private void OnEnable()
-        {
-            if (_playerInput == null) return;
-            _playerInput.gameActions.Player.Dash.performed += OnDash;
-        }
-
-        private void OnDisable()
-        {
-            _playerInput.gameActions.Player.Dash.performed -= OnDash;
         }
     }
 }
